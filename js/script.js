67b386ef775bd7732631a7e2e9dfcc9f1ad680ba
maciej <63735702+MrMaciejM@users.noreply.github.com>
2022-12-24 03:25:42 +0100

    added ability to render city names dynamically in the main dashboard

--- js/script.js
+++ js/script.js
@@ -4,39 +4,57 @@
 
 // ? = parameter list
 
+var searchInput = document.getElementById("search-input");
+var searchBtn = document.getElementById("search-button");
+var searchForm = document.getElementById("search-form");
+
 var apiKey = "7790a821eef7813b147ba03c132c329b";
-var city = "London"; // users input
 var baseURL = "https://api.openweathermap.org/data/2.5/";
 var currentURL = baseURL + `weather?appid=${apiKey}&units=metric&`;
 var forecastURL = baseURL + `forecast?appid=${apiKey}&units=metric&`;
 var iconURL = "https://openweathermap.org/img/w/";
 
-// icon weather property - icon, e.g. icon: "50n"
-function inputSubmitted(cityName) {
-  // server side request
+// get city coordinates
+function getCurrentCast(cityName) {
   // prettier-ignore
-  $.get(currentURL + `q=${cityName}`)
+  return $.get(currentURL + `q=${cityName}`)
+}
+
+function getInput() {
+  searchForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // prettier-ignore
+    getCurrentCast(searchInput.value)
+     .then(function (city) {
+        //console.log(city); // display all objects 
+
+        // display location name
+        // <h4 id="currentHeader">London (14/09/2022) .png</h4>
+        var cityName = city.name; 
+        var currentTime =  moment().format("DD/MM/YYYY");
+        var currentIcon = city.weather[0].icon + ".png";
+        // icon had its own img URL?? 
+ 
+        // need to set img tag with jQuery 
+        $("#today").prepend(`<h4>${cityName} (${currentTime}) ${iconURL + city.weather[0].icon}.png </h4>`);
+
+        console.log(currentIcon)
+        console.log(city)
+     })
+  });
+}
+getInput();
+
+// display current weather on main dashboard
+
+// get city, current date and icon
+
+//getInput();
+// ***************************
+/*
+$.get(currentURL + `q=${cityName}`)
    .then(function (currentData) {
     // shows current weather
     console.log(
         currentData         
-    ) // currentData . coord: {lon: -0.1257, lat: 51.5085} 
-
-    console.log(`
-    Temp: ${Math.round(currentData.main.temp)}
-    Humidity: ${currentData.main.humidity}%
-    Wind: ${currentData.wind.speed}
-    IconURL: ${iconURL + currentData.weather[0].icon}.png}
-    `);
-
-    // show 5 day forecast - you need to loop through only 5 
-    $.get(forecastURL + `lat=${currentData.coord.lat}&lon=${currentData.coord.lon}`) 
-    .then(function(forecastData) {
-        for (var castObj of forecastData.list) {
-            //console.log(`${iconURL + castObj.weather[0].icon}.png`)
-        }
-    //console.log(forecastData); 
-    })
-  });
-}
-inputSubmitted(city);
+    ) // currentData . coord: {lon: -0.1257, lat: 51.5085} */
